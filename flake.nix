e9ece5affe74dc720cd2ee64a5ba5ac291024c70
CoKoala <ali@capsai.org>
2023-09-20 09:21:16 +0000

    chore: flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev env for web servers";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs@{nixpkgs, flake-parts, ...}: 
+    flake-parts.lib.mkFlake { inherit inputs; }{
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem = {pkgs, system, self', inputs', config, ... }: {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [nodePackages_latest.typescript-language-server bun nodejs_20];
+        };
+      };
+    };
+}
